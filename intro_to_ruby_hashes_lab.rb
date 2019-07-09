c2bbca5abdaa41908e754552e13ae34edcee45e3
Alice Ly Ms <alicehly@gmail.com>
2019-07-09 21:40:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,9 @@ monopoly = {
   :railroads=>{
     :pieces=> 4,
     :names=>{},
-    :rent_in_dollars=>{ :one_piece_owned=> 25,},
+    :rent_in_dollars=>{ :one_piece_owned=> 25,
+                        :two_pieces_owned=> 50
+    },
   }
 }
 end
